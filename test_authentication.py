f0d9ef607fc12c7e62c19d425b702a66cbd50dcf
Michael Jordan <michael@glowingpixel.com>
2021-05-28 19:12:42 -0700

    Channel tests, updates for reorganized code

--- test_authentication.py
+++ test_authentication.py
@@ -1,47 +1,47 @@
 import unittest
-from adderlib import adder
+from adderlib import adder, users, channels, devices, urlhandlers
 
 class TestAuthentication(unittest.TestCase):
 
 	def test_valid_login(self):
-		user = adder.AdderUser()
-		ad = adder.AdderAPI(user=user, url_handler=adder.DebugHandler(), api_version=8)
+		user = users.AdderUser()
+		ad = adder.AdderAPI(user=user, url_handler=urlhandlers.DebugHandler(), api_version=8)
 		ad.login("test","goodpwd")
 		self.assertTrue(user.logged_in)
 		self.assertFalse(user.logged_out)
 	
 	def test_invalid_login(self):
-		user = adder.AdderUser()
-		ad = adder.AdderAPI(user=user, url_handler=adder.DebugHandler(), api_version=8)
+		user = users.AdderUser()
+		ad = adder.AdderAPI(user=user, url_handler=urlhandlers.DebugHandler(), api_version=8)
 		ad.login("test","badpwd")
 		self.assertFalse(user.logged_in)
 		self.assertTrue(user.logged_out)
 	
 	def test_valid_transmitter(self):
-		user = adder.AdderUser()
-		ad = adder.AdderAPI(user=user, url_handler=adder.DebugHandler(), api_version=8)
+		user = users.AdderUser()
+		ad = adder.AdderAPI(user=user, url_handler=urlhandlers.DebugHandler(), api_version=8)
 		ad.login("test","goodpwd")
 		
 		tx = next(ad.getTransmitters())
-		self.assertIsInstance(tx, adder.AdderTransmitter)
+		self.assertIsInstance(tx, devices.AdderTransmitter)
 		self.assertTrue(tx, tx.interfaces[0].is_online)
 	
 	def test_valid_receiver(self):
-		user = adder.AdderUser()
-		ad = adder.AdderAPI(user=user, url_handler=adder.DebugHandler(), api_version=8)
+		user = users.AdderUser()
+		ad = adder.AdderAPI(user=user, url_handler=urlhandlers.DebugHandler(), api_version=8)
 		ad.login("test","goodpwd")
 
 		rx = next(ad.getReceivers())
-		self.assertIsInstance(rx, adder.AdderReceiver)
+		self.assertIsInstance(rx, devices.AdderReceiver)
 		self.assertTrue(rx, rx.is_connected)
 	
 	def test_valid_channel(self):
-		user = adder.AdderUser()
-		ad = adder.AdderAPI(user=user, url_handler=adder.DebugHandler(), api_version=8)
+		user = users.AdderUser()
+		ad = adder.AdderAPI(user=user, url_handler=urlhandlers.DebugHandler(), api_version=8)
 		ad.login("test","goodpwd")
 
 		chan = next(ad.getChannels())
-		self.assertIsInstance(chan, adder.AdderChannel)
+		self.assertIsInstance(chan, channels.AdderChannel)
 
 if __name__ == "__main__":
 	unittest.main()
