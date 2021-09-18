da716d55255300a44e07c132a1c79a90bee42d9c
Michael Jordan <michael@glowingpixel.com>
2021-09-17 19:55:35 -0700

    Add Requests-based URL handler

--- adderlib/urlhandlers.py
+++ adderlib/urlhandlers.py
@@ -1,22 +1,41 @@
 import abc, urllib.parse
+import requests, requests.compat
 import xmltodict
-
 
 class UrlHandler(abc.ABC):
 	"""Abstract URL Handler"""
 
 	@classmethod
 	@abc.abstractmethod
-	def api_call(url) -> dict:
+	def api_call(url:str) -> dict:
 		"""Handle a call to the REST API and return a dictionary result"""
 		pass
 
+class RequestsHandler(UrlHandler):
+
+	def __init__(self, address:str, port:int=80):
+		self._server = str(address)
+		self._port = int(port)
+	
+	def api_call(self, url:str) -> xmltodict.OrderedDict:
+		"""GET a call to the API"""
+		response = requests.get(self._abs_url(url))
+		
+		if not response.ok:
+			raise Exception(f"Error contacting {url}: Returned {response.status_code}")
+
+		return xmltodict.parse(response.content).get("api_response")
+	
+	def _abs_url(self, rel_url:str) -> str:
+		"""Return the absolute URL for an API call"""
+		# TODO: Do better
+		return f"http://{self._server}:{self._port}/{rel_url}"
 
 class DebugHandler(UrlHandler):
 	
 	"""URL handler for debugging with sample XMLs from the documentation"""
 	@classmethod
-	def api_call(cls, url) -> dict:
+	def api_call(cls, url) -> xmltodict.OrderedDict:
 
 		params = urllib.parse.parse_qs(url)
 		print(f"Requesting: {url}")
